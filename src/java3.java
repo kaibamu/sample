4b0f7b11343ae5ac0f3e4b7ff1d1e773ec2e6525
kikuchikouhei <kikuchikouhei@10.95.3.96>
2025-05-20 12:05:11 +0900

    演習２

--- src/java3.java
+++ src/java3.java
@@ -1,5 +1,5 @@
 public class java3 {
-    public static void main(String... args) {
-System.out.println("Hello world");
-    }
-}
+	   public static void main(String... args) {
+	        System.out.println("paiza");
+	    }
+	}
