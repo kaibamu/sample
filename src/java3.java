2f72fec61269f38930370c20ef2e01aca1bbbc3e
kikuchikouhei <kikuchikouhei@10.95.3.96>
2025-05-20 13:01:02 +0900

    演習問題まとめ 入門１から入門４まで

--- src/java3.java
+++ src/java3.java
@@ -1,6 +1,69 @@
-public class java3 {
+public class Main {
 	   public static void main(String... args) {
 	        System.out.println("paiza");
 	    }
 	}
-//演習問題
+
+//02:mainメソッドを持つクラスの作成
+public class Main {
+    public static void main(String...args) {
+        System.out.println("Hello paiza");
+}
+}
+
+//03:インデント
+public class Main {
+    public static void main(String... args) {
+        System.out.println("Hello paiza");
+        
+    }
+}
+//01:標準出力を扱うプログラム
+public class Main {
+    public static void main(String... args) {
+        System.out.println("paiza");
+    }
+}
+//02:System.out.println を使った標準出力への出力
+public class Main {
+    public static void main(String... args) {
+        System.out.println("paiza");
+        System.out.println("learning");
+    }
+}
+
+//03:System.out.print を使った標準出力への出力
+public class Main {
+    public static void main(String... args) {
+        System.out.print("paiza");
+        System.out.print("learning");
+    }
+}
+
+//01:単一行のコメント（//）
+public class Main {
+    public static void main(String... args) {
+        //「paiza」と出力
+        System.out.println("paiza");
+    }
+}
+public class Main {
+    public static void main(String... args) {
+        //System.out.println("Hello");
+        System.out.println("paiza");
+    }
+}
+
+//02:複数行のコメント（/*~*/）
+/*
+サンプルコード Main
+開発者:霧島
+作成日:20xx年x月
+*/
+public class Main {
+    public static void main(String... args) {
+        System.out.println("paiza");
+    }
+}
+
+//新・Java入門編1: プログラミングとJavaについて学習しよう~新・Java入門編4: コメントの付け方を学習しようまでの演習問題
